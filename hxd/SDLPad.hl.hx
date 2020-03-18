bc9ad151dcf54000db06402ca261994816adf1dc
Nonagon <45755256+9-gon@users.noreply.github.com>
2020-03-18 18:57:51 -0500

    Delete SDLPad.hl.hx

--- hxd/SDLPad.hl.hx
+++
@@ -1,9 +0,0 @@
-package hxd;
-
-class SDLPad extends Pad
-{
-    public function new () super();
-
-    public function getInputName (input:Int) :String return Pad.CONFIG_SDL.names[input];
-    public function getInputStatus (input:Int) :Float return this.values[input];
-}
