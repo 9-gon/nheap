a36504a5556bfd1ee5da1d44987486c1b9a4392d
nonagon <rupuguyt@gmail.com>
2020-03-18 18:23:36 -0500

    change function names

--- hxd/SDLPad.hl.hx
+++ hxd/SDLPad.hl.hx
@@ -5,5 +5,5 @@ class SDLPad extends Pad
     public function new () super();
 
     public function getInputName (input:Int) :String return Pad.CONFIG_SDL.names[input];
-    public function getStatus (input:Int) :Float return this.values[input];
+    public function getInputStatus (input:Int) :Float return this.values[input];
 }
